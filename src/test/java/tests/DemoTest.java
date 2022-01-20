7903d68a59f86eca12ca51ba7a0392dc1138e341
vemulathrinath <thrinath.vemula@gmail.com>
2022-01-20 20:52:25 +0530

    1st commit

---
+++ src/test/java/tests/DemoTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+    @Test
+    public void demoTest1(){
+        System.out.println("Demo test");
+    }
+
+    @Test(description = "test for fail test demo")
+    public void demoFailTest(){
+        System.out.println("test for fail test demo");
+        Assert.assertEquals(25,25);
+
+    }
+
+}
